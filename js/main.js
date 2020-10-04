b7f2b188d1ef5221605f0822061099be97b90dc6
yoonsangkwak <yunsang08@naver.com>
2020-10-04 15:16:23 +0900

    activate scene

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 (() => {
 
     let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
     
-    const screneInfo = [
+    const sceneInfo = [
         {
             // 0
             type: 'sticky',
@@ -43,23 +46,49 @@
 
     function setLayout() {
         // 각 스크롤 섹션의 높이 세팅
-        for (let i = 0; i < screneInfo.length; i++) {
-            screneInfo[i].scrollHeight = screneInfo[i].heightNum * window.innerHeight;
-            screneInfo[i].objs.container.style.height = `${screneInfo[i].scrollHeight}px`;
+        for (let i = 0; i < sceneInfo.length; i++) {
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
 
+        yOffset = window.pageYOffset;
+        let totalScrollHeight = 0;
+        for (let i = 0; i < sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
     function scrollLoop() {
+        prevScrollHeight = 0;
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
         
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }
+
+        if (yOffset < prevScrollHeight) {
+            if (currentScene === 0) return; // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+            currentScene --;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }
+
     }
     
-    window.addEventListener('resize', setLayout);
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
         scrollLoop();
     });
-
-    setLayout();
+    
+    // DOMContentLoaded : HTML구조만 로드되면 바로 실행, 실행타이밍 빠름
+    window.addEventListener('load', setLayout);
+    window.addEventListener('resize', setLayout);
 
 })();
