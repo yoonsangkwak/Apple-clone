83fcf0b4bf48b40a54225e9dfa8ce45df32d343b
yoonsangkwak <yunsang08@naver.com>
2020-10-08 17:38:12 +0900

    wrap up second scroll section

--- js/main.js
+++ js/main.js
@@ -64,9 +64,16 @@
                 messageB: document.querySelector('#scroll-section-2 .b'),
                 messageC: document.querySelector('#scroll-section-2 .c'),
                 pinB: document.querySelector('#scroll-section-2 .b .pin'),
-                pinC: document.querySelector('#scroll-section-2 .c .pin')
+                pinC: document.querySelector('#scroll-section-2 .c .pin'),
+                canvas: document.querySelector('#video-canvas-1'),
+                context: document.querySelector('#video-canvas-1').getContext('2d'),
+                videoImages: []
             },
             values: {
+                videoImageCount: 960,
+                imageSequence: [0, 959],
+                canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
+                canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
                 messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
                 messageB_translateY_in: [30, 0, { start: 0.6, end: 0.65 }],
                 messageC_translateY_in: [30, 0, { start: 0.87, end: 0.92 }],
@@ -106,6 +113,13 @@
             imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
             sceneInfo[0].objs.videoImages.push(imgElem);
         }
+
+        let imgElem2;
+        for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+            imgElem2 = new Image();
+            imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
+            sceneInfo[2].objs.videoImages.push(imgElem2);
+        }
     }
 
     function setLayout() {
@@ -133,6 +147,7 @@
 
         const heightRatio = window.innerHeight / 1080;
         sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
+        sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
 
     }
 
@@ -177,7 +192,7 @@
                 let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
                 objs.context.drawImage(objs.videoImages[sequence], 0, 0);
                 objs.canvas.style.opacity = calcValues(values.canvas_opacity, currentYOffset);
-
+                
                 if (scrollRatio <= 0.22) {
                     // in
                     objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
@@ -187,7 +202,7 @@
                     objs.messageA.style.opacity = calcValues(values.messageA_opacity_out, currentYOffset);
                     objs.messageA.style.transform = `translate3d(0, ${calcValues(values.messageA_translateY_out, currentYOffset)}%, 0)`;
                 }
-    
+                
                 if (scrollRatio <= 0.42) {
                     // in
                     objs.messageB.style.opacity = calcValues(values.messageB_opacity_in, currentYOffset);
@@ -197,7 +212,7 @@
                     objs.messageB.style.opacity = calcValues(values.messageB_opacity_out, currentYOffset);
                     objs.messageB.style.transform = `translate3d(0, ${calcValues(values.messageB_translateY_out, currentYOffset)}%, 0)`;
                 }
-    
+                
                 if (scrollRatio <= 0.62) {
                     // in
                     objs.messageC.style.opacity = calcValues(values.messageC_opacity_in, currentYOffset);
@@ -207,7 +222,7 @@
                     objs.messageC.style.opacity = calcValues(values.messageC_opacity_out, currentYOffset);
                     objs.messageC.style.transform = `translate3d(0, ${calcValues(values.messageC_translateY_out, currentYOffset)}%, 0)`;
                 }
-    
+                
                 if (scrollRatio <= 0.82) {
                     // in
                     objs.messageD.style.opacity = calcValues(values.messageD_opacity_in, currentYOffset);
@@ -217,13 +232,23 @@
                     objs.messageD.style.opacity = calcValues(values.messageD_opacity_out, currentYOffset);
                     objs.messageD.style.transform = `translate3d(0, ${calcValues(values.messageD_translateY_out, currentYOffset)}%, 0)`;
                 }
-    
+                
                 break;
-    
-            case 2:
-                // console.log('2 play');
-                if (scrollRatio <= 0.32) {
-                    // in
+                
+                case 2:
+                    let sequence2 = Math.round(calcValues(values.imageSequence, currentYOffset));
+                    objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
+
+                    if (scrollRatio <= 0.5) {
+                        // in
+                        objs.canvas.style.opacity = calcValues(values.canvas_opacity_in, currentYOffset);
+                    } else {
+                        // out
+                        objs.canvas.style.opacity = calcValues(values.canvas_opacity_out, currentYOffset);
+                    }
+
+                    if (scrollRatio <= 0.32) {
+                        // in
                     objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
                     objs.messageA.style.transform = `translate3d(0, ${calcValues(values.messageA_translateY_in, currentYOffset)}%, 0)`;
                 } else {
@@ -243,7 +268,7 @@
                     objs.messageB.style.opacity = calcValues(values.messageB_opacity_out, currentYOffset);
                     objs.pinB.style.transform = `scaleY(${calcValues(values.pinB_scaleY, currentYOffset)})`;
                 }
-    
+                
                 if (scrollRatio <= 0.93) {
                     // in
                     objs.messageC.style.transform = `translate3d(0, ${calcValues(values.messageC_translateY_in, currentYOffset)}%, 0)`;
